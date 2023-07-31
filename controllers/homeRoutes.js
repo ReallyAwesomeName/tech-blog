2d83a49399a5801842171d1442026f41b3fb5061
ReallyAwesomeName <90869629+ReallyAwesomeName@users.noreply.github.com>
2023-07-30 23:46:51 -0400

    okay actually done with routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
 // controllers/homeRoutes.js
 
 const router = require("express").Router();
-const { Post, User, Comment } = require("../../models");
+const { Post, User, Comment } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
@@ -9,8 +9,38 @@ router.get("/", async (req, res) => {
     const postData = await Post.findAll({
       include: [User],
     });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render("homepage", { posts, logged_in: req.session.logged_in });
   } catch (err) {
     console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/signup", async (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup");
+});
+
+router.get("/login", async (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [User, Comment],
+    });
+    const post = postData.get({ plain: true });
+    res.render("post", { post, logged_in: req.session.logged_in });
+  } catch (err) {
     res.status(500).json(err);
   }
 });
---
+++ controllers/profileRoutes.js
@@ -0,0 +1,33 @@
+// controllers/profileRoutes.js
+
+const router = require("express").Router();
+const auth = require("../utils/auth");
+const { Post } = require("../models/");
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render("profile", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/newpost", auth, async (req, res) => {
+  try {
+    res.render("newpost", {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
